904254da3a20c04d2d017f449572e71387e7abe3
iver <iverrog@gmail.com>
2025-01-03 01:09:34 +0100

    code clean up

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-#![allow(unused)]
 mod stl_op;
 mod utils;
 use utils::Blender;
@@ -16,7 +15,7 @@ fn main(){
 
     blender.show();
 }
-
+#[allow(unused)]
 fn straight_skeleton(blender:&mut Blender) {
     //let vertices = vec![
     //    Point2::new(0.0,0.0),
@@ -33,7 +32,7 @@ fn straight_skeleton(blender:&mut Blender) {
         Point2::new(0.7,1.0),
         Point2::new(0.0,1.1),
     ];
-    //let vertices = vec![
+    //let vertices:Vec<Point2<f32>> = vec![
     //    Point2::new(40., 52.0),
     //    Point2::new(62.5,42.5),
     //    Point2::new(50.0,32.5),
@@ -42,63 +41,40 @@ fn straight_skeleton(blender:&mut Blender) {
     //    Point2::new(25.0,40.),
     //    Point2::new(20.0,20.0),
     //    Point2::new(10.0,50.)
-    //];
-    let weights:Vec<f32> = vertices.iter().map(|x| 1.0 ).collect();
+    //].into_iter().rev().collect();
+    let weights:Vec<f32> = vertices.iter().map(|_| 1.0 ).collect();
     let vertices_as_f32:Vec<[f32;3]> = vertices.clone().into_iter().map(|p|[ p[0],p[1], 0.0 ]).collect();
 
     match straight_skeleton::create_skeleton(vertices.clone(), &weights){
         Ok(skeleton) =>{
-            let mut skel_points:Vec<[f32;2]> = skeleton.vertices.iter()
+            let skel_points:Vec<[f32;2]> = skeleton.vertices.iter()
                 .map(|x| [x[0],x[1]])
                 .collect::<Vec<[f32;2]>>();
-            println!("skel_points: {skel_points:?}");
-
-            let mut skel_edges:Vec<[usize;2]> = skeleton.edges.clone()
-                .into_iter()
-                .filter(|[p1,p2]| p1 != p2)
-                .collect();
-            println!("skel_edges: {skel_edges:?}");
 
             blender.line_body(
                 &skel_points,
-                skel_edges
+                skeleton.edges
                 );
         }
         Err(error) => println!("\x1b[031m{error}\x1b[0m")
     }
-
-
-
     blender.edge_loop_points(&vertices_as_f32);
-    
 }
-
-
+#[allow(unused)]
 fn pipe_line(blender:&mut Blender){
-    let profiles = stl_op::main(blender);
-    let first_profile = profiles.iter().next().unwrap();
+    let contours = stl_op::main(blender);
+    let first_contour = contours.iter().next().unwrap();
 
-    let profile:Vec<Point2<f32>> = first_profile.to_owned().iter()
+    let contour:Vec<Point2<f32>> = first_contour.to_owned().iter()
             .map(|vec| Point2::new(vec[0],vec[1]))
             .collect();
 
-    let weights: Vec<f32> = profile.iter().map(|_| 1.0 ).collect();
+    let weights: Vec<f32> = contour.iter().map(|_| 1.0 ).collect();
 
-    println!("profile points: {}",
-        profile.len(),
-        );
-    let skeleton = straight_skeleton::create_skeleton(profile.clone(),&weights).unwrap();
-
-    println!("Points: {} | edges: {} | larges point index:{}",
-        skeleton.vertices.len(),
-        skeleton.edges.len(),
-        skeleton.edges.iter().map(|[p1,p2]| max(p1,p2)).max().unwrap()
-        );
-    //dbg!(&skeleton);
+    let skeleton = straight_skeleton::create_skeleton(contour.clone(),&weights).unwrap();
 
     blender.line_body(
         &skeleton.vertices.iter().map(|x| [x[0],x[1]]).collect::<Vec<[f32;2]>>(),
         skeleton.edges.clone()
         );
 }
-
