b0c8bf6fd6d90d97e44b6b72536a7875fa1f0459
iver <iverrog@gmail.com>
2025-04-17 01:56:55 +0200

    First step in infill generation overhal

--- src/gcode/mod.rs
+++ src/gcode/mod.rs
@@ -1,10 +1,12 @@
-use nalgebra::{Point2, Point3};
+use nalgebra::{Point, Point2, Point3, Matrix2};
+use priority_queue::core_iterators::Iter;
 use stl_io::IndexedMesh;
 
 use std::iter::Iterator;
 use std::io::BufReader;
 use std::fs::{self, File};
 use std::iter;
+use std::iter::zip;
 
 use crate::geo::*;
 use crate::stl_op;
@@ -72,7 +74,10 @@ pub fn main(blender:&mut Blender) {
     let brim = 25;
 
     // let mesh_layer_dir = "../curving_overhang/p0.2";
-    let mesh_layer_dir = "../simple_overhang";
+    // let mesh_layer_dir = "../simple_overhang";
+    // let mesh_layer_dir = "../wine_glass";
+    let mesh_layer_dir = "../flat_wine_glass";
+
     let mut mesh_layers = import_layers(&mesh_layer_dir)
         .map(|layer_mesh| contour_and_mesh_colider_from_mesh(layer_mesh));
 
@@ -288,17 +293,24 @@ fn generate_infill_allong_y(bounds:Polygon,mesh:&MeshCollider) -> Vec<Path> {
     return collums
 }
 
-fn generate_infill_allong_x(bounds:Polygon,mesh:&MeshCollider) -> Vec<Path> {
-    let aabb = &bounds.outer_loop().aabb;
-    let (min,max) = ( aabb.x_min.floor() as isize, aabb.x_max.ceil() as isize);
-    // let (min,max) = ( aabb.x_min.ceil() as isize, aabb.x_max.floor() as isize);
+#[derive(Copy,Clone,Debug)]
+enum PntType {
+    Perimeter,
+    Inside
+}
 
-    let offset = -aabb.x_min.floor();
-    let range = (max-min) as usize;
-    let mut yz_vals:Vec<Vec<(f32,Option<f32>)>> = vec![Vec::new();range+1];
-
-    for (e1,e2) in mesh.edges.iter()
-        .map(|edge|(mesh.vertices[edge.0],mesh.vertices[edge.1]))
+fn sample_points<'a,I>(
+    edges:I,
+    aabb:&AABB,
+    range:usize,
+    min:isize,
+    yz_vals:&mut Vec<Vec<(f32,f32,PntType)>>,
+    tag:PntType)
+where 
+    I: Iterator<Item = (Point3<f32>,Point3<f32>)> + 'a,
+{
+    for (e1,e2) in edges
+        // .map(|edge|(mesh.vertices[edge.0],mesh.vertices[edge.1]))
         .map(|(e1,e2)| if e1.x < e2.x { (e1,e2) }else{ (e2,e1) }){
 
             let residual = e1.x.ceil()-e1.x;
@@ -332,45 +344,39 @@ fn generate_infill_allong_x(bounds:Polygon,mesh:&MeshCollider) -> Vec<Path> {
                 .filter_map(|(x_ndx,y,z)| usize::try_from(x_ndx).ok().map(|x_ndx|(x_ndx,y,z)) )
                 .filter(|(x_ndx,y,z)| *x_ndx < range+1)
                 .filter(|(x_ndx,y,z)| (aabb.y_min <= *y) && (*y <= aabb.y_max) )
-                .for_each(|(x_ndx,y,z)| yz_vals[x_ndx].push((y,Some(z))) )
+                .for_each(|(x_ndx,y,z)| yz_vals[x_ndx].push((y,z,tag)) )
     }
+}
 
-    // Add contour intersections
-    for (e1,e2) in bounds.all_edges()
-        .map(|(e1,e2)| if e1.x < e2.x { (e1,e2) }else{ (e2,e1) }){
+fn generate_infill_allong_x(bounds:Polygon,mesh:&MeshCollider) -> Vec<Path> {
 
-            let residual = e1.x.ceil()-e1.x;
+    let aabb = &bounds.outer_loop().aabb;
+    let (min,max) = ( aabb.x_min.floor() as isize, aabb.x_max.ceil() as isize);
 
-            let ay = (e2.y-e1.y)/(e2.x-e1.x);
-            let by = (e1.y) + ay*residual;
+    let offset = -aabb.x_min.floor();
+    let range = (max-min) as usize;
+    let mut yz_vals:Vec<Vec<(f32,f32,PntType)>> = vec![Vec::new();range+1];
 
-            let (x_min,x_max) = ((e1.x.ceil() as isize) , (e2.x.ceil() as isize));
+    let edges = mesh.edges.iter()
+        .map(|edge|(mesh.vertices[edge.0],mesh.vertices[edge.1]));
 
-            let x_vals = (0..(x_max - x_min)).map(|x| x as f32);
+    let tag = PntType::Inside;
 
-            let y = match ay == 0.0 {
-                true => vec![e1.y; x_vals.len()],
-                false => x_vals.clone().map(|x| ay * x + by ).collect(),
-            };
+    sample_points(edges, aabb, range, min, &mut yz_vals, tag);
 
-            y.into_iter()
-                .enumerate()
-                .map(|(i,y)|{ 
-                    let x_ndx = i as isize + x_min - min;
-                    (x_ndx,y)
-                })
-                .filter_map(|(x_ndx,y)| usize::try_from(x_ndx).ok().map(|x_ndx|(x_ndx,y)) )
-                .filter(|(x_ndx,y)| *x_ndx < range+1)
-                .filter(|(x_ndx,y)| (aabb.y_min <= *y) && (*y <= aabb.y_max) )
-                .for_each(|(x_ndx,y)| yz_vals[x_ndx].push((y,None)) )
-        }
+    let bounds3d = bounds.project_onto(mesh);
+    let edges = bounds3d.all_edges()
+        .map(|(e1,e2)|(e1*1.,e2*1.));
+    let tag = PntType::Perimeter;
 
-    // sort the table:
+    sample_points(edges, aabb, range, min, &mut yz_vals, tag);
+
+    // sort the tables by y value in alternating increasing/decrasing order:
     for (i,collumn) in yz_vals.iter_mut().enumerate(){
         if i%2 == 0 {
-            collumn.sort_unstable_by(|(t1,_),(t2,_)| t1.partial_cmp(t2).unwrap());
+            collumn.sort_unstable_by(|(y1,_,_),(y2,_,_)| y1.partial_cmp(y2).unwrap());
         } else {
-            collumn.sort_unstable_by(|(t1,_),(t2,_)| t2.partial_cmp(t1).unwrap());
+            collumn.sort_unstable_by(|(y1,_,_),(y2,_,_)| y2.partial_cmp(y1).unwrap());
         }
     }
 
@@ -382,22 +388,22 @@ fn generate_infill_allong_x(bounds:Polygon,mesh:&MeshCollider) -> Vec<Path> {
         let mut segment = Vec::new();
         let mut within_bounds = false;
 
-        for (y,z) in yz {
-            match z {
+        for (y,z,tag) in yz {
+            match tag {
                 // found a mesh point keep it if it is inside the contur
-                Some(z) => if within_bounds {
+                PntType::Inside => if within_bounds {
                     segment.push(Point3::new(x,y,z));
                 },
                 // point on the bounding contour which implies crossing 
                 // the boundary between inside and outside the bounds
-                None => {
-                    let point3d = project_point_onto(&Point2::new(x,y),&mesh);
+                PntType::Perimeter => {
+                    let point = Point3::new(x,y,z);
                     if within_bounds { // completed the line segment
-                        segment.push( point3d );
+                        segment.push( point );
                         collums.push( Path{points:segment} );
                         segment = Vec::new();
                     } else {
-                        segment.push(point3d)
+                        segment.push(point)
                     }
                     within_bounds = !within_bounds;
                     (y,0.0);
