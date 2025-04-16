1b65ec5ed5c75e940b9679ad7c50e6ce9cc18ca6
iver <iverrog@gmail.com>
2025-04-16 14:50:04 +0200

    Make Paht::shorten_ends more preformant

--- src/gcode/path.rs
+++ src/gcode/path.rs
@@ -1,4 +1,6 @@
-use nalgebra::Point3;
+use core::todo;
+
+use nalgebra::{Point, Point3};
 use crate::geo::Contour3d;
 
 pub enum PathType{
@@ -19,14 +21,39 @@ impl From<Contour3d> for Path {
     }
 }
 impl Path{
-    // Shortens the start and end of the path by the offset amout.
+    pub fn edges(&self) -> impl Iterator<Item = (&Point3<f32>,&Point3<f32>)> {
+        let points = self.points.iter();
+        let points_offset_by_one = points.clone().skip(1);
+        points.zip(points_offset_by_one)
+    }
+
     pub fn shorten_ends(&mut self, offset:f32) {
-        let half_offset = offset/2.0;
+        self.shorten_end(offset/2.0);
+        self.shorten_start(offset/2.0);
+    }
 
-        // if  (self.points[1] - self.points[0]).magnitude() < half_offset  || 
-        //     (self.points[last_ndx-1] - self.points[last_ndx]).magnitude() < half_offset
-        //     {panic!("end vertices to close together for shorten ends to work correctly")}
+    pub fn shorten_start(&mut self, offset:f32) {
+        let mut removed_length = 0.;
+        let mut first_edge_length;
+        let mut remove_ndx = None;
 
+        for (i,(first,next)) in self.edges().enumerate() {
+            first_edge_length = (next - first).magnitude();
+            if removed_length + first_edge_length > offset {
+                remove_ndx = Some(i);
+                break;
+            }
+            removed_length += first_edge_length;
+        }
+
+        self.points.drain(0..remove_ndx.expect("path is shorter than the desired offset"));
+
+        let end_vec = (self.points[1] - self.points[0]).normalize();
+        *self.points.first_mut().expect("path is shorter than the desired offset") 
+            += end_vec * (offset-removed_length);
+    }
+
+    pub fn shorten_end(&mut self, offset:f32) {
         // shorten end
         let mut removed_length = 0.;
         let mut last_edge_length;
@@ -36,30 +63,13 @@ impl Path{
             let next_last = self.points[self.points.len()-2];
             last_edge_length = (next_last - last).magnitude();
             removed_length + last_edge_length
-        } < half_offset {
+        } < offset {
             self.points.pop().expect("path is shorter than the desired offset");
             removed_length += last_edge_length;
         }
         let end_vec = (self.points[self.points.len()-2] - self.points[self.points.len()-1]).normalize();
         
-        *self.points.last_mut().expect("path is shorter than the desired offset") += end_vec * (half_offset-removed_length);
-
-        // shorten start
-        let mut removed_length = 0.;
-        let mut first_edge_length;
-
-        while {
-            let first = self.points[0];
-            let next = self.points[1];
-            first_edge_length = (next - first).magnitude();
-            removed_length + first_edge_length
-        } < half_offset {
-            // NOTE: Very inefficient
-            self.points.remove(0);
-            removed_length += first_edge_length;
-        }
-        let end_vec = (self.points[1] - self.points[0]).normalize();
-        
-        *self.points.first_mut().expect("path is shorter than the desired offset") += end_vec * (half_offset-removed_length);
+        *self.points.last_mut().expect("path is shorter than the desired offset") 
+            += end_vec * (offset-removed_length);
     }
 }
