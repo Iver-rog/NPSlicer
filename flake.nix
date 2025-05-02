6ec36f87b305a8ab0f1c96d070bd3543fb89af5e
iver <iverrog@gmail.com>
2025-05-02 11:46:55 +0200

    Move from cargo2nix to native nix devshell with oxalica rust overlay to get newer rustc version
    A more updated rust compiler was needed in order
    to use a newer version of the i_overlay crate.
    This change is in preparation for upcomming
    tagged_boolean functionality which requires features
    from i_overlay version 3.0 to work.
    
    The new flake.nix file does not provide a build output
    as it did previously, as a result nix run command will
    no longer work.

--- flake.nix
+++ flake.nix
@@ -1,27 +1,79 @@
 {
+  description = "A devShell example";
+
   inputs = {
-    cargo2nix.url = "github:cargo2nix/cargo2nix/";
-    flake-utils.follows = "cargo2nix/flake-utils";
-    nixpkgs.follows = "cargo2nix/nixpkgs";
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
   };
 
-  outputs = inputs: with inputs;
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
-          inherit system;
-          overlays = [cargo2nix.overlays.default];
+          inherit system overlays;
         };
+      in
+      {
+        devShells.default = with pkgs; mkShell rec{
+          buildInputs = [
+            freetype
+            expat
+            fontconfig
+            openssl
+            pkg-config
+            eza
+            fd
+            rust-bin.beta.latest.default
 
-        rustPkgs = pkgs.rustBuilder.makePackageSet {
-          rustVersion = "1.75.0";
-          packageFun = import ./Cargo.nix;
-        };
+            # necessary for building wgpu in 3rd party packages (in most cases)
+            libxkbcommon
+            wayland xorg.libX11 xorg.libXcursor xorg.libXrandr xorg.libXi
+            alsa-lib
+            fontconfig freetype
+            shaderc directx-shader-compiler
+            pkg-config cmake
+            mold # could use any linker, needed for rustix (but mold is fast)
 
-      in rec {
-        packages = {
-          layer-gen-rs = (rustPkgs.workspace.layer-gen-rs {});
-          default = packages.layer-gen-rs;
+            libGL
+            vulkan-headers vulkan-loader
+            vulkan-tools vulkan-tools-lunarg
+            vulkan-extension-layer
+            vulkan-validation-layers # don't need them *strictly* but immensely helpful
+
+            # necessary for developing (all of) wgpu itself
+            cargo-nextest cargo-fuzz
+
+            # nice for developing wgpu itself
+            typos
+
+            # if you don't already have rust installed through other means,
+            # this shell.nix can do that for you with this below
+            yq # for tomlq below
+            rustup
+
+            # nice tools
+            gdb rr
+            evcxr
+            valgrind
+            renderdoc
+
+
+          ];
+
+          shellHook = ''
+            alias ls=eza
+            alias find=fd
+
+            export RUSTC_VERSION="$(tomlq -r .toolchain.channel rust-toolchain.toml)"
+            export PATH="$PATH:''${CARGO_HOME:-~/.cargo}/bin"
+            export PATH="$PATH:''${RUSTUP_HOME:-~/.rustup/toolchains/$RUSTC_VERSION-x86_64-unknown-linux/bin}"
+            export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${builtins.toString (pkgs.lib.makeLibraryPath buildInputs)}";
+
+            rustup default $RUSTC_VERSION
+            rustup component add rust-src rust-analyzer
+          '';
         };
       }
     );
