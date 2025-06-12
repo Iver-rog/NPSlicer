6f20e599e7928439329b1dbee16b67d256a85c3d
iver <iverrog@gmail.com>
2025-06-13 00:50:39 +0200

    Code cleanup

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,8 @@ use std::time::Instant;
 /// WARNING: all content in the TMP_DIR is deleted each time the project is run
 const TEMP_DIR: &str = "/home/iver/Documents/NTNU/prosjekt/layer-gen-rs/tmp/";
 
+const t_min:f32 = 20.0;
+
 fn main(){
     let start_time = Instant::now();
     // let mut args = env::args();
@@ -39,11 +41,12 @@ fn main(){
     let mut blender = Blender::new();
 
     let settings = Settings{
-        layer_height: 0.4,
+        layer_height:0.4,
+        perimeter_line_width: 1.,
         infill_percentage: 20,
-        nr_of_perimeters: 3,
-        // brim: 0,
-        // outer_wall_first: false,
+        nr_of_perimeters: 2,
+        brim: 0,
+        outer_wall_first: false,
         translate_xy: [50., 50.],
         // feedrates:Feedrates{
         //     initial_layer:800,
@@ -56,28 +59,46 @@ fn main(){
     dbg!(&settings);
 
     let stl_path = "../mesh/simple_overhang.stl";
-    // let stl_path = "../mesh/internal_external.stl";
     // let stl_path = "../mesh/bunny.stl";
 
+    // let stl_path = "../mesh/internal_external.stl";
+    // let stl_path = "../mesh/circular overhang.stl";
+    // let stl_path = "../mesh/2.stl";
+    // let stl_path = "../mesh/rapport demo.stl";
+    // let stl_path = "../mesh/curved overhang.stl";
     // let stl_path = "../mesh/pipe.stl";
     // let stl_path = "../mesh/curved overhang.stl";
     // let stl_path = "../mesh/wine_glass3.stl";
 
     blender.load_mesh(stl_path,"input mesh");
     let layer_perimeters = extract_planar_layers_from_mesh(stl_path,&settings);
+    // let d_x2 = (settings.overhang_angle*0.5).tan()*settings.layer_height;
+    // let p0_offset = ss_offset(layer_perimeters[0].clone(),-0.5*d_x2);
+    // blender.display2d(&p0_offset[0], 0.0, "c0-offset2", "er jeg dum?");
+    // blender.display2d(&layer_perimeters[0][0], 1.0, "c0", "er jeg dum?");
+        // .enumerate()
+        // .flat_map(|(i,layer)|layer.iter().map(move |p|(i,p)))
+        // .for_each(|(i,polygon)|blender.display2d(polygon, i as f32, "layer contour", "layer contours"));
+    // panic!();
 
     mesh_gen(&mut blender,&layer_perimeters,&settings);
     // get_user_confimation();
     crate_or_clear_dir(TEMP_DIR);
-    blender.export_layers(TEMP_DIR);
-    blender.ping(); //make sure blender has processed all messages
     let mesh_gen_time = start_time.elapsed();
 
-    let start_time = Instant::now();
+    blender.export_layers(TEMP_DIR);
+    blender.ping(); //make sure blender has processed all messages
+    let boolean_time = start_time.elapsed();
+
+    // let start_time = Instant::now();
     gcode::main(&mut blender,TEMP_DIR,&settings);
     let gcode_gen_time = start_time.elapsed();
-    println!("mesh generation time {} sec",mesh_gen_time.as_secs_f64());
-    println!("gcode generation time {} sec",gcode_gen_time.as_secs_f64());
+
+    
+    println!("mesh generation time  {} sec",mesh_gen_time.as_secs_f64());
+    println!("mesh booleon time     {} sec",(boolean_time-mesh_gen_time).as_secs_f64());
+    println!("gcode generation time {} sec",(gcode_gen_time-boolean_time).as_secs_f64());
+    println!("\x1b[034mExported G-code file to {TEMP_DIR}gcode.gcode\x1b[0m");
 }
 
 fn crate_or_clear_dir<T:AsRef<std::path::Path>>(tmp:T){
@@ -145,7 +166,7 @@ fn generate_layer(
                 .map(|p|{let mut p = p.clone(); p.invert(); p})
                 .collect();
 
-            let skeleton = match skeleton::skeleton_from_polygons_with_limit(polygons.clone(),30.0){
+            let skeleton = match skeleton::skeleton_from_polygons_with_limit(polygons.clone(),t_min){
                 Ok(skeleton) => skeleton,
                 Err(err) => {
                     println!("\x1b[031m{err}\x1b[0m");
@@ -165,6 +186,7 @@ fn generate_layer(
 }
 
 fn mesh_gen(blender:&mut Blender, layers:&Vec<Vec<Polygon>>, s:&Settings){
+    let mut avr_support_ratio = 1.;
 
     let theta = s.overhang_angle;
     let layer_h = s.layer_height;
@@ -180,6 +202,7 @@ fn mesh_gen(blender:&mut Blender, layers:&Vec<Vec<Polygon>>, s:&Settings){
         }
     }
     let last_layer = layers.len() - 1;
+    let boundary_clone = layers.clone();
 
     let mut layers = layers.into_iter();
     let mut prev_support:Vec<Polygon> = layers.next().unwrap().clone();
@@ -212,11 +235,24 @@ fn mesh_gen(blender:&mut Blender, layers:&Vec<Vec<Polygon>>, s:&Settings){
             println!("Error: failed to generate layer {layer_nr:03}-000-layer");
         }
 
-        let min_support_ratio = if layer_nr == last_layer {0.99} else {0.5};
+        let min_support_ratio = if layer_nr == last_layer {0.95} else {0.5};
         let mut i = 0;
-        while ( support_a / perimeter_a ) < min_support_ratio {
+        while {
+            match boundary_clone.get(layer_nr+1){
+                Some(b_next) => {
+                    let (overlap,_) = tagged_boolean(b_next.clone(), support.clone(), OverlayRule::Intersect);
+                    let overlap_a:f32 = overlap.iter().map(|polygon|polygon.area()).sum();
+                    let b_next_a:f32 = b_next.iter().map(|polygon|polygon.area()).sum();
+                    (overlap_a/b_next_a)<0.5
+                },
+                None => (support_a / perimeter_a) < 0.95,
+            } 
+        }{
+        // while ( support_a / perimeter_a ) < min_support_ratio {
+        
             i += 1;
-            // if i > 3 {break}
+            // if (layer_nr != last_layer)&&(i>3){break}
+            if i > 15 {break}
             let offset_sup = ss_offset(support.clone(),-d_x1);
 
             let (support2,tags) = tagged_boolean(layer.clone() ,offset_sup, OverlayRule::Intersect);
